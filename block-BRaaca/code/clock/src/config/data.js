5622b0e30ddba5a97b37e36e4fbd050dcbfc2faf
Shadab Ali <shadabali604@gmail.com>
2021-02-07 11:02:22 +0530

    added: completed

---
+++ block-BRaaca/code/clock/src/config/data.js
@@ -0,0 +1,40 @@
+let clocks = [
+  {
+    id: 1,
+    city: "Boston",
+    tz: "America/New_York",
+    enabled: true,
+  },
+  {
+    id: 2,
+    city: "Dublin",
+    tz: "Europe/Dublin",
+    enabled: true,
+  },
+  {
+    id: 3,
+    city: "Taipei",
+    tz: "Asia/Taipei",
+    enabled: true,
+  },
+  {
+    id: 4,
+    city: "Rome",
+    tz: "Europe/Rome",
+    enabled: true,
+  },
+  {
+    id: 5,
+    city: "Sydney",
+    tz: "Australia/Sydney",
+    enabled: true,
+  },
+  {
+    id: 6,
+    city: "Helsinki",
+    tz: "Europe/Helsinki",
+    enabled: true,
+  },
+];
+
+export default clocks;
